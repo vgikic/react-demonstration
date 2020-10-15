ef2305e44398e5dbb064b36d8bdaa7788b3273c5
Vladimir Gikić <gikicvladimir@gmail.com>
2020-10-15 22:20:37 +0200

    .

--- react-demonstration/src/components/header/header.tsx
+++ react-demonstration/src/components/header/header.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import style from './Header.module.scss';
+
 import { AccountRoute, ItemsRoute, LoginRoute } from '../../routes';
 
 const Header = () => {
+
+    const materialIconStyle = `material-icons ${style['md-light']} ${style['md-48']}`;
+
+    const [routeState, changerouteState] = useState(`${style.route}`);
+
+    const onMenuClick = () => {
+        const nextState = routeState.indexOf('active') > -1 ? `${style.route}` : `${style.route} ${style.active}`;
+        changerouteState(nextState);
+    }
+
     return (
-        <header>
-            <nav>
+        <header className={style.container}>
+            <nav className={style.navigation}>
                 <ul>
-                    <li>
-                        <Link to={`${AccountRoute}`}>Account</Link>
+
+                    <li className={routeState}>
+                        <Link to={`${ItemsRoute}`}>ITEMS</Link>
                     </li>
-                    <li>
-                        <Link to={`${LoginRoute}`}>Log-in</Link>
+
+                    <li className={routeState}>
+                        <Link to={`${LoginRoute}`}>LOGIN</Link>
                     </li>
-                    <li>
-                        <Link to={`${ItemsRoute}`}>Items - TODO</Link>
+
+                    <li className={routeState}>
+                        <Link to={`${AccountRoute}`}>ACCOUNT</Link>
                     </li>
+
+                    <li className="icon-container">
+                        <i onClick={onMenuClick} className={materialIconStyle}>view_headline</i>
+                    </li>
+                    
                 </ul>
             </nav>
         </header>
