540ecaed9906bf980b913949e1849ab2d61f5830
Vladimir Gikić <gikicvladimir@gmail.com>
2020-10-16 00:03:02 +0200

    .

--- react-demonstration/src/components/header/header.tsx
+++ react-demonstration/src/components/header/header.tsx
@@ -5,37 +5,41 @@ import style from './Header.module.scss';
 import { AccountRoute, ItemsRoute, LoginRoute } from '../../routes';
 
 const Header = () => {
-
+    debugger;
     const materialIconStyle = `material-icons ${style['md-light']} ${style['md-48']}`;
 
-    const [routeState, changerouteState] = useState(`${style.route}`);
+    const [navigationState, changeNavigationState] = useState({ routeStyle: `${style.route}`, navStyle: `` });
 
     const onMenuClick = () => {
-        const nextState = routeState.indexOf('active') > -1 ? `${style.route}` : `${style.route} ${style.active}`;
-        changerouteState(nextState);
+        const isActive = navigationState.routeStyle.indexOf('active') != -1;
+
+        const nextRouteStyle = isActive ? `${style.route}` : `${style.route} ${style.active}`;
+        const nextNavStyle = isActive ? '' : `${style['navigation-extend']}`;
+        changeNavigationState({ navStyle: nextNavStyle, routeStyle: nextRouteStyle });
+
     }
 
     return (
         <header className={style.container}>
-            <nav className={style.navigation}>
+            <nav className={navigationState.navStyle}>
                 <ul>
 
-                    <li className={routeState}>
+                    <li className={navigationState.routeStyle}>
                         <Link to={`${ItemsRoute}`}>ITEMS</Link>
                     </li>
 
-                    <li className={routeState}>
+                    <li className={navigationState.routeStyle}>
                         <Link to={`${LoginRoute}`}>LOGIN</Link>
                     </li>
 
-                    <li className={routeState}>
+                    <li className={navigationState.routeStyle}>
                         <Link to={`${AccountRoute}`}>ACCOUNT</Link>
                     </li>
 
                     <li className="icon-container">
                         <i onClick={onMenuClick} className={materialIconStyle}>view_headline</i>
                     </li>
-                    
+
                 </ul>
             </nav>
         </header>
